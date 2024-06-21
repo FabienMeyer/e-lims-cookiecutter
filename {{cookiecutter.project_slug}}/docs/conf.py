e2a1f541752c98fbdce09707ea4d6b7a74c79b01
Fabien Meyer <fabien-meyer@outlook.com>
2024-06-21 18:58:18 +0200

    add github pages deploy

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -4,8 +4,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
-import {{ cookiecutter.project_slug }}
-
 # -- General configuration ---------------------------------------------
 extensions = [
 	'sphinx.ext.autodoc',
